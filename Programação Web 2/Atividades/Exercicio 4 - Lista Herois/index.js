f1adea58b9bb1351d95600aeb4ff3610cd469547
Lucas Orestes Fabris <33981828+Lucas-OF28@users.noreply.github.com>
2022-06-18 12:21:13 -0300

    Add files via upload

---
+++ Programação Web 2/Atividades/Exercicio 4 - Lista Herois/index.js
@@ -0,0 +1,178 @@
+var listaSuperherois = [
+    {
+        "superhero":"Batman", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Bruce Wayne",
+        "first_appearance":"Detective Comics #27",
+        "characters":"Bruce Wayne"
+    },
+    {
+        "superhero":"Superman", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Kal-El",
+        "first_appearance":"Action Comics #1",
+        "characters":"Kal-El"
+    },
+    {
+        "superhero":"Flash", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Jay Garrick",
+        "first_appearance":"Flash Comics #1",
+        "characters":"Jay Garrick, Barry Allen, Wally West, Bart Allen"
+    },
+    {
+        "superhero":"Green Lantern", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Alan Scott",
+        "first_appearance":"All-American Comics #16",
+        "characters":"Alan Scott, Hal Jordan, Guy Gardner, John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz"
+    },
+    {
+        "superhero":"Green Arrow", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Oliver Queen",
+        "first_appearance":"More Fun Comics #73",
+        "characters":"Oliver Queen"
+    },
+    {
+        "superhero":"Wonder Woman", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Princess Diana",
+        "first_appearance":"All Star Comics #8",
+        "characters":"Princess Diana"
+    },
+    {
+        "superhero":"Martian Manhunter", 
+        "publisher":"DC Comics", 
+        "alter_ego":"J'onn J'onzz",
+        "first_appearance":"Detective Comics #225",
+        "characters":"Martian Manhunter"
+    },
+    {
+        "superhero":"Robin/Nightwing", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Dick Grayson",
+        "first_appearance":"Detective Comics #38",
+        "characters":"Dick Grayson"
+    },
+    {
+        "superhero":"Blue Beetle", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Dan Garret",
+        "first_appearance":"Mystery Men Comics #1",
+        "characters":"Dan Garret, Ted Kord, Jaime Reyes"
+    },
+    {
+        "superhero":"Black Canary", 
+        "publisher":"DC Comics", 
+        "alter_ego":"Dinah Drake",
+        "first_appearance":"Flash Comics #86",
+        "characters":"Dinah Drake, Dinah Lance"
+    },
+    {
+        "superhero":"Spider Man", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Peter Parker",
+        "first_appearance":"Amazing Fantasy #15",
+        "characters":"Peter Parker"
+    },
+    {
+        "superhero":"Captain America", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Steve Rogers",
+        "first_appearance":"Captain America Comics #1",
+        "characters":"Steve Rogers"
+    },
+    {
+        "superhero":"Iron Man", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Tony Stark",
+        "first_appearance":"Tales of Suspense #39",
+        "characters":"Tony Stark"
+    },
+    {
+        "superhero":"Thor", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Thor Odinson",
+        "first_appearance":"Journey into Myster #83",
+        "characters":"Thor Odinson"
+    },
+    {
+        "superhero":"Hulk", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Bruce Banner",
+        "first_appearance":"The Incredible Hulk #1",
+        "characters":"Bruce Banner"
+    },
+    {
+        "superhero":"Wolverine", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"James Howlett",
+        "first_appearance":"The Incredible Hulk #180",
+        "characters":"James Howlett"
+    },
+    {
+        "superhero":"Daredevil", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Matthew Michael Murdock",
+        "first_appearance":"Daredevil #1",
+        "characters":"Matthew Michael Murdock"
+    },
+    {
+        "superhero":"Hawkeye", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Clinton Francis Barton",
+        "first_appearance":"Tales of Suspense #57",
+        "characters":"Clinton Francis Barton"
+    },
+    {
+        "superhero":"Cyclops", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Scott Summers",
+        "first_appearance":"X-Men #1",
+        "characters":"Scott Summers"
+    },
+    {
+        "superhero":"Silver Surfer", 
+        "publisher":"Marvel Comics", 
+        "alter_ego":"Norrin Radd",
+        "first_appearance":"The Fantastic Four #48",
+        "characters":"Norrin Radd"
+    }
+]
+
+//Map - Lista de Alter Ego
+var listaAlterEgo = listaSuperherois.map(function(item){
+    return item.alter_ego
+})
+
+console.log(listaAlterEgo)
+
+//Reduce - Tamanho total de caracteres de todos os first_appearance cujo publisher é Marvel Comics
+var tamanhoCaracteres = listaSuperherois.reduce(function(valorAnterior, item){
+    if(item.publisher == "Marvel Comics"){
+        return valorAnterior + item.first_appearance.length
+    } else {
+        return valorAnterior
+    }
+}, 0)
+
+console.log(tamanhoCaracteres)
+
+//Filter - Lista com personagem cujo publisher é DC Comics
+var personagensDcComics = listaSuperherois.filter(function(item){
+    return item.publisher == "DC Comics"
+})
+
+console.log(personagensDcComics);
+
+//Sort - Organizar vetor pelo tamanho dos nomes (superhero)
+var tamanhoNomes = listaSuperherois.sort(function(a, b){
+    if (a.superhero.length > b.superhero.length){
+        return 1
+    } else {
+        return -1
+    }
+})
+
+console.log(listaSuperherois)
